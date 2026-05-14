864e3badec38274837d37b85de0a9ab2c4fb125a
Joycet2 Tian <joyce.tian@kes.global>
2026-05-14 05:35:44 +0000

    clean model structure + equivalent_items_list

--- models/Model/intermediate/equivalent_items_list.sql
+++ models/Model/intermediate/equivalent_items_list.sql
@@ -1,3 +1,7 @@
+{{ config(
+    materialized='table'
+) }}
+
 SELECT
     a.*,
     b."item_full" AS matched_equivalent_item,
@@ -13,9 +17,12 @@ SELECT
     b."is_active",
     b."ready_to_obsolete",
     b."ENTITY",
-    c."item_description" AS New_Item_description
+    c."item_description" AS new_item_description
+
 FROM KES_PRD_LNCE_DB.INTEGRATION.STG_LNCE__TXN_ITEM_BUSINESS_DOCUMENT a
+
 LEFT JOIN KES_WRKSPC_GSC_DB.INTEGRATION.DKU_ITEM_CLEANUP_DETAILS b
     ON TRIM(a.EXTERNAL_ITEM_CODE) = TRIM(b."item_full")
+
 LEFT JOIN KES_WRKSPC_GSC_DB.INTEGRATION.DKU_ITEM_CLEANUP_DETAILS c
     ON TRIM(a.ITEM_CODE) = TRIM(c."item_full")
